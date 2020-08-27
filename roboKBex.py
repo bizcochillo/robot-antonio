73a71619cf722a1252c1e43d5223f114696110c6
bizcochillo <abelardop@gmail.com>
2020-08-28 00:36:15 +0200

    Add joystick functionality and robo script for engine

---
+++ roboKBex.py
@@ -0,0 +1,85 @@
+class Robot:
+    def __init__(self):
+        try:
+            import RPi.GPIO as GPIO
+        except RuntimeError:
+            print("Error importing RPi.GPIO!  This must be run as root using sudo")
+        self.GPIO = GPIO
+        self.GPIO.setmode(GPIO.BCM)
+        #I-B: Iquierda para detras
+        self.GPIO.setup(17, GPIO.OUT)
+        #I-F. Iquierda para adelante
+        self.GPIO.setup(18, GPIO.OUT)
+        #D-B Derecha para atras
+        self.GPIO.setup(22, GPIO.OUT)
+        #Derecha para adelante
+        self.GPIO.setup(23, GPIO.OUT)
+
+
+    def adelante(self):
+        self.GPIO.output(17, False)
+        self.GPIO.output(18, True)
+        self.GPIO.output(22, False)
+        self.GPIO.output(23, True)
+
+
+    def atras(self):
+        self.GPIO.output(17, True)
+        self.GPIO.output(18, False)
+        self.GPIO.output(22, True)
+        self.GPIO.output(23, False)
+
+    def izquierda(self):
+        self.GPIO.output(17, False)
+        self.GPIO.output(18, True)
+        self.GPIO.output(22, False)
+        self.GPIO.output(23, False)
+
+    def derecha(self):
+        self.GPIO.output(17, False)
+        self.GPIO.output(18, False)
+        self.GPIO.output(22, False)
+        self.GPIO.output(23, True)
+
+
+    def parar(self):
+        self.GPIO.output(17, False)
+        self.GPIO.output(18, False)
+        self.GPIO.output(22, False)
+        self.GPIO.output(23, False)
+    def getch(self):
+        import sys, tty, termios
+        old_settings = termios.tcgetattr(0)
+        new_settings = old_settings[:]
+        new_settings[3] &= ~termios.ICANON
+        try:
+            termios.tcsetattr(0, termios.TCSANOW, new_settings)
+            ch = sys.stdin.read(1)
+        finally:
+            termios.tcsetattr(0, termios.TCSANOW, old_settings)
+        return ch
+
+robot = Robot();
+while True:
+    print("Adelante: E. ");
+    print("Atras:    C.");
+    print("Iquierda: S.");
+    print("Derecha:  F.");
+    print("Fin: P");
+    nb = robot.getch();
+    if nb == b'E' or nb == b'e':
+        print("Adelante\n");
+        robot.adelante();
+    elif nb == b'c' or nb == b'C':
+        print("Atras\n");
+        robot.atras();
+    elif nb == b'S' or nb == b's':
+        print("Iquierda\n");
+        robot.izquierda();
+    elif nb == b'F' or nb == b'f':
+        print("Derecha\n");
+        robot.derecha()
+    elif nb == b'p' or nb == b'P':
+        print("Fin\n");
+        robot.parar();
+        break
--- static/app.js
+++ static/app.js
@@ -1,3 +1,6 @@
+const MOTION_THREESHOLD = 20
+let socket;
+
 $(document).ready(function() {
     $.getJSON('config.json', function(data) {
         //HERE YOu need to get the name server
@@ -10,14 +13,43 @@ $(document).ready(function() {
         container: document.getElementById('container'),
         mouseSupport: true,
     });
-    joystick.addEventListener('touchStart', function() {
-        console.log('down')
-    })
-    joystick.addEventListener('touchEnd', function() {
-        console.log('up')
-    })
+    var isListening = false;
+    var lastOrder = 'stop';
+    var order = undefined;
+    $(document).bind('touchstart mousedown', function() {
+        isListening = true;
+        console.log('start listening');
+    });
+
+    $(document).bind('touchend mouseup', function() {
+        isListening = false;
+        sendNewOrder('stop');
+        lastOrder = 'stop';
+        console.log('stop listening');
+    });
+
 
     setInterval(function() {
+        if (!isListening) return; // not listening for events. 
+        let deltaX = joystick.deltaX();
+        let deltaY = joystick.deltaY();
+        if (!orderTriggered(deltaX, deltaY)) {
+            // motion does not hit threeshold. 
+            order = "stop";
+        } else {
+            if (Math.abs(deltaX) > Math.abs(deltaY)) {
+                // right or left
+                order = deltaX > 0 ? "right" : "left";
+            } else {
+                // up or down
+                order = deltaY > 0 ? "down" : "up";
+            }
+        }
+        if (order !== lastOrder) {
+            sendNewOrder(order)
+            lastOrder = order;
+        }
+
         var outputEl = document.getElementById('result');
         outputEl.innerHTML = '<b>Result:</b> ' +
             ' dx:' + joystick.deltaX() +
@@ -29,8 +61,17 @@ $(document).ready(function() {
     }, 1 / 30 * 1000);
 });
 
+function orderTriggered(x, y) {
+    return (Math.abs(x) > MOTION_THREESHOLD) || (Math.abs(y) > MOTION_THREESHOLD);
+}
+
+function sendNewOrder(o) {
+    console.log("--> new order: " + o);
+    socket.send('message', o);
+}
+
 function connectToSocket(server) {
-    var socket = io.connect('http://' + server + ':5000', { 'forceNew': true });
+    socket = io.connect('http://' + server + ':5000', { 'forceNew': true });
 
     socket.on('message', function(data) {
         console.log('JavaScript client connected');
